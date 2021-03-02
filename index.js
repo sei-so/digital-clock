2e547b9d22c64e64bc6fcd9d4f61b7ca4ceee3c5
seiso <viktoreydman@gmail.com>
2021-03-02 21:23:09 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let clock = () => {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+    
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').innerText = time;
+
+    setTimeout(clock, 1000);
+}
+
+clock();
